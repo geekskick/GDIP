c385b26ac203e06acae535bf92943c77c6aad46c
geekskick <geeks_kick@yahoo.co.uk>
2017-01-22 17:27:43 +0000

    global display implemented

--- FreeRTOS/Demo/CORTEX_CY8C5588_PSoC_Creator_GCC/FreeRTOS_Demo.cydsn/Custom Tasks/Display/globaldisplay.c
+++ FreeRTOS/Demo/CORTEX_CY8C5588_PSoC_Creator_GCC/FreeRTOS_Demo.cydsn/Custom Tasks/Display/globaldisplay.c
@@ -31,28 +31,108 @@ static QueueHandle_t xDispQueue;
 static xComPortHandle xComPort;
 static xTaskHandle xComTask;
 
-/************************************************************/
+/*-----------------------------------------------------------------------------*/
 /* internal functions and a point to them mean I wont have to repeat myself 
     when writing code to access the mutex etc
 */
 void vGetCom( xComPortHandle *out );
 void vSetCom( xComPortHandle *in );
+void vComBase( void ( *comFunct ) ( xComPortHandle *xCom ), xComPortHandle *xArg );
 
 void vGetTask( xTaskHandle *out );
 void vSetTask( xTaskHandle *in );
+void vTaskBase( void ( *taskFunct ) ( xTaskHandle *xTask ), xTaskHandle *xArg );
 
 void vGetQ( QueueHandle_t *out );
 void vSetQ( QueueHandle_t *in );
-
-void vComBase( void ( *comFunct ) ( xComPortHandle *xCom ), xComPortHandle *xArg );
-void vTaskBase( void ( *taskFunct ) ( xTaskHandle *xTask ), xTaskHandle *xArg );
 void vQueueBase( void ( *QFunct ) ( QueueHandle_t *xCom ), QueueHandle_t *xArg );
+
+/*-----------------------------------------------------------------------------*/
+void vComBase( void ( *comFunct ) ( xComPortHandle *xCom ), xComPortHandle *xArg )
+{
+    /* If the mutex hasn't been initialised then don't allow access to the resource, 
+     * make an attempt to create the mutex, and if it's created call the function again. 
+    */
+    if( xInitialised )
+    {
+        if( pdTRUE == xSemaphoreTake( xGatekeeper, ( TickType_t ) 20 ) ) // 20 ms block time arbitrarily picked for now
+        {
+            comFunct( xArg );
+            xSemaphoreGive( xGatekeeper );
+        }
+    }
+    else 
+    {
+        /* here it isn't initialised so initialise the mutex, and if successful recursively
+        attempt to perform the action again */
+        xGatekeeper = xSemaphoreCreateMutex();
+        if( xGatekeeper != NULL )
+        {
+            xInitialised = true;
+            vQueueBase( comFunct, xArg );
+        }
+    } 
+}
+
+/*-----------------------------------------------------------------------------*/
+void vTaskBase( void ( *taskFunct ) ( xTaskHandle *xTask ), xTaskHandle *xArg )
+{
+    /* If the mutex hasn't been initialised then don't allow access to the resource, 
+     * make an attempt to create the mutex, and if it's created call the function again. 
+    */
+    if( xInitialised )
+    {
+        if( pdTRUE == xSemaphoreTake( xGatekeeper, ( TickType_t ) 20 ) ) // 20 ms block time arbitrarily picked for now
+        {
+            taskFunct( xArg );
+            xSemaphoreGive( xGatekeeper );
+        }
+    }
+    else 
+    {
+        /* here it isn't initialised so initialise the mutex, and if successful recursively
+        attempt to perform the action again */
+        xGatekeeper = xSemaphoreCreateMutex();
+        if( xGatekeeper != NULL )
+        {
+            xInitialised = true;
+            vQueueBase( taskFunct, xArg );
+        }
+    } 
+}
+
+/*-----------------------------------------------------------------------------*/
+void vQueueBase( void ( *QFunct ) ( QueueHandle_t *xCom ), QueueHandle_t *xArg )
+{
+    /* If the mutex hasn't been initialised then don't allow access to the resource, 
+     * make an attempt to create the mutex, and if it's created call the function again. 
+    */
+    if( xInitialised )
+    {
+        if( pdTRUE == xSemaphoreTake( xGatekeeper, ( TickType_t ) 20 ) ) // 20 ms block time arbitrarily picked for now
+        {
+            QFunct( xArg );
+            xSemaphoreGive( xGatekeeper );
+        }
+    }
+    else 
+    {
+        /* here it isn't initialised so initialise the mutex, and if successful recursively
+        attempt to perform the action again */
+        xGatekeeper = xSemaphoreCreateMutex();
+        if( xGatekeeper != NULL )
+        {
+            xInitialised = true;
+            vQueueBase( QFunct, xArg );
+        }
+    }      
+}
 
 /*------------------------------------------------------------------*/
 xTaskHandle xGetDisplayTaskHandle( void )
 {
     xTaskHandle rc;
-    
+    vTaskBase( &vGetTask, &rc );
     return rc;
 }
 
@@ -60,6 +140,7 @@ xTaskHandle xGetDisplayTaskHandle( void )
 QueueHandle_t xGetDisplayInputQueue( void )
 {
     QueueHandle_t rc;
+    vQueueBase( &vGetQ, &rc );
     return rc;
 }
 
@@ -67,27 +148,26 @@ QueueHandle_t xGetDisplayInputQueue( void )
 xComPortHandle xGetDisplayComPortHandle( void )
 {
     xComPortHandle rc;
-    
+    vComBase( &vGetCom, &rc );
     return rc;
 }
 
 /*------------------------------------------------------------------*/
 void vSetDisplayTaskHandle( xTaskHandle xNewHandle )
 {
-    ( void ) xNewHandle;
-    
+    vTaskBase( &vSetTask, &xNewHandle );  
 }
 
 /*------------------------------------------------------------------*/
 void vSetDisplayInputQueue( QueueHandle_t xNewQueue )
 {
-    ( void ) xNewQueue;
+    vQueueBase( &vSetQ, &xNewQueue );
 }
 
 /*------------------------------------------------------------------*/
 void vSetDisplayComPortHandle( xComPortHandle xNewHandle )
 {
-    ( void )xNewHandle;
+    vComBase( &vSetCom, &xNewHandle );
     
 }
     
