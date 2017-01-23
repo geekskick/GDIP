e4f52020585567c014906c2db9ca067ed33942e0
geekskick <geeks_kick@yahoo.co.uk>
2017-01-23 18:18:38 +0000

    2 Buttons in Decoder task

--- FreeRTOS/Demo/CORTEX_CY8C5588_PSoC_Creator_GCC/FreeRTOS_Demo.cydsn/Custom Tasks/Input Decoder/decodertask.c
+++ FreeRTOS/Demo/CORTEX_CY8C5588_PSoC_Creator_GCC/FreeRTOS_Demo.cydsn/Custom Tasks/Input Decoder/decodertask.c
@@ -21,18 +21,25 @@
 #include "decodertask.h"
 #include "partest.h"
 
-QueueHandle_t xKPOutputQueue = NULL;
+/* the keyad queue is sending an ascii character, so convert it to a number
+ * but masking the lower nibble to turn it from a 0x61 = a, into 0x01 etc
+ */
+#define MAP_IDX(val) ( ( uint8_t )val &0x0F )
+
+QueueHandle_t xDecoderOutputQueue = NULL;
 QueueHandle_t xKeypadInputQueue = NULL;
 
 /*-----------------------------------------------------------------------*/
 static portTASK_FUNCTION_PROTO( vDecoderTask, pvParameters );
+void prvCreateServoMovementStruct( xServoNumber_t xServo, xServoDirection_t xDirectionToMove, xServoQueueParams_t *pxQueueArgs );
+
 
 /*-----------------------------------------------------------------------*/
 static portTASK_FUNCTION( vDecoderTask, pvParamaters )
 {
 char8 cButton;                      /* the button pressed */
 bool bSend = false;                 /* if we are sending a value to the servo task then this is true, else false */
-xServoQueueParams_t xToServo;  /* sending stuff to the servos means it needs to be packaged up into a struct */
+xServoQueueParams_t xToServo;       /* sending stuff to the servos means it needs to be packaged up into a struct */
 ( void ) pvParamaters;              /* get rid of warnings */
     
 for(;;)
@@ -40,21 +47,24 @@ for(;;)
         /* do nothing until something is received in the queue */
         if( pdTRUE == xQueueReceive( xKeypadInputQueue, &cButton, portMAX_DELAY ) )
         {
-            /* I only have one servo for now */
-            xToServo.xServo = BaseRotation;
+            bSend = true;
             
             switch( cButton )
             {
                 case 'a':
-                    xToServo.xDirection = ADD;
-                    bSend = true;
+                    prvCreateServoMovementStruct( BaseRotation, ADD, &xToServo );
                     break;
                 case 'b':
-                    xToServo.xDirection = SUB;
-                    bSend = true;
+                    prvCreateServoMovementStruct( BaseRotation, SUB, &xToServo );
+                    break;
+                case 'c':
+                    prvCreateServoMovementStruct( BaseElevation, ADD, &xToServo );
+                    break;
+                case 'd':
+                    prvCreateServoMovementStruct( BaseElevation, SUB, &xToServo );
                     break;
                 default:
-                /* all other button pressed don't mean anything with one servo */
+                /* all other button pressed don't mean anything servo */
                     bSend = false;
                     break;
                 
@@ -62,7 +72,7 @@ for(;;)
             
             if( true == bSend )
             {
-                if( pdFALSE == xQueueSend( xKPOutputQueue, ( void* )&xToServo, ( TickType_t ) portMAX_DELAY ) )
+                if( pdFALSE == xQueueSend( xDecoderOutputQueue, ( void* )&xToServo, ( TickType_t ) portMAX_DELAY ) )
                 {
                     /* error sending to the servo queue */
                 }
@@ -77,8 +87,15 @@ QueueHandle_t xStartDecoderTask( int priority, xDecoderParams_t xParams )
     /* create the input q and attache the output queue */
     xKeypadInputQueue = xQueueCreate( DECODER_INPUT_QUEUE_SIZE, sizeof(signed char) );
     xTaskCreate( vDecoderTask, "Decoder", configMINIMAL_STACK_SIZE, ( void * ) &xParams, priority, NULL );
-    xKPOutputQueue = *(xParams.pxDecoderOutputQueue);
-    return xKPOutputQueue;
+    xDecoderOutputQueue = *(xParams.pxDecoderOutputQueue);
+    return xDecoderOutputQueue;
+}
+
+/*-----------------------------------------------------------------------*/
+void prvCreateServoMovementStruct( xServoNumber_t xServo, xServoDirection_t xDirectionToMove, xServoQueueParams_t *pxQueueArgs )
+{
+    pxQueueArgs->xDirection = xDirectionToMove;
+    pxQueueArgs->xServo = xServo;
 }
 
 /* [] END OF FILE */
